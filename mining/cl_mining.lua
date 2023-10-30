c617e11f59abc2bd17587b70b39fd86cd2db377f
underscore <95720805+speedsunderscore@users.noreply.github.com>
2023-10-30 21:42:42 +0000

    Buy and Sell Button

--- mining/cl_mining.lua
+++ mining/cl_mining.lua
@@ -1,14 +1,19 @@
 Mining = Mining or {}
 
 
-LocalPlayer().Minerals = {}
+net.Receive("Mining.AddMinerals", function()
 
-for k, v in pairs(Mining.Minerals) do
+    local minerals = net.ReadTable()
 
-    LocalPlayer().Minerals[v.class] = 2
+    LocalPlayer().Minerals = {}
 
+    for k, v in pairs(minerals) do
 
-end
+        LocalPlayer().Minerals[k] = v
+
+    end
+
+end)
 
 
 net.Receive("Mining.UseBench", function()
@@ -81,8 +86,9 @@ net.Receive("Mining.UseBench", function()
 
             draw.SimpleText(v.maxAmt .. " max", CheadleUI.GetFont("Montserrat", 16), w - 3, 18*2, Color(221,221,221), TEXT_ALIGN_RIGHT, TEXT_ALIGN_TOP)
             
+            draw.SimpleText("B:" .. formatMoney(Mining.Minerals[v.class].price * 4), CheadleUI.GetFont("Montserrat", 15), 1, h, Color(221,221,221), TEXT_ALIGN_LEFT, TEXT_ALIGN_BOTTOM)
 
-            draw.SimpleText("$" .. v.price .. " per mineral", CheadleUI.GetFont("Montserrat", 15), w/2, h - 2, Color(221,221,221), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM)
+            draw.SimpleText("S:$" .. formatMoney(Mining.Minerals[v.class].price), CheadleUI.GetFont("Montserrat", 15), w - 1, h, Color(221,221,221), TEXT_ALIGN_RIGHT, TEXT_ALIGN_BOTTOM)
 
         end
 
@@ -105,6 +111,15 @@ net.Receive("Mining.UseBench", function()
         CheadleUI.SetPos(sellButton, 98, 2, 82, 4)
 
         CheadleUI.HoverEffect(sellButton, Color(15,15,15), Color(35,35,35), 200)
+
+
+        local buyButton = CheadleUI.Button(oreDraw, "Buy", CheadleUI.GetFont("Montserrat", 20), Color(35,35,35), Color(221,221,221))
+
+        CheadleUI.SetSize(buyButton, 40, 25)
+
+        CheadleUI.SetPos(buyButton, 2, 0, 82, 4)
+
+        CheadleUI.HoverEffect(buyButton, Color(15,15,15), Color(35,35,35), 200)
 
     end
 
